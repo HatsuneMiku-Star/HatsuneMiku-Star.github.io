6c6613bd7a47cace9125f0f595746abba9936ed6
Zhangsd <627756772@qq.com>
2022-06-23 17:32:58 +0800

    Site updated: 2022-06-23 17:32:56

---
+++ js/FunnyTitle.js
@@ -0,0 +1,17 @@
+<!--浏览器搞笑标题-->
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = '哼~';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = 'ヾ(◍°∇°◍)欢迎回来';
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
